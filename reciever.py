e774f9c24928c1b1801ed4f09664a559f11edd85
Merlin <merlinhancock@gmail.com>
2022-10-26 12:53:34 +0100

    Seperate login function

--- reciever.py
+++ reciever.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, Request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import sqlalchemy as sa
+from typing import Optional
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dispensers.db'
 # init db
@@ -9,9 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dispensers.db'
 db = SQLAlchemy(app) # db. = sa. but sa gives annotations
 
 #db.init_app(app)
-
-
-
 class Dispensers(db.Model):
     __tablename__ = "dispensers_table"
 
@@ -35,7 +34,26 @@ class ScheduleTimes(db.Model):
         return f"<name {self.id}>"
 
 
-subscribers = []
+def check_login(request: Request) -> Optional[bool]:
+    '''Check info provided in request is correct to login'''
+    # Check if users request has the dispenser id
+    if (str_id := request.values.get('id')) == None:
+        return 'No ID provided'
+
+    # Attempt to decode dispenser id to base10 from str hex
+    try:
+        id = int(str_id,16)
+    except:
+        return 'Invalid dispenser id format'
+    # Check if users request has the passkey
+    if (passkey := request.values.get('pass')) == None:
+        return 'No pass provided'
+    # Check if the dispenser id matches an existing dispenser
+    if (dispenser:= db.session.get(Dispensers, id)) == None:
+        return 'Invalid dispenser id'
+    # Check if the actual dispenser's key matches that provided by the user
+    if (dispenser.passkey != passkey):
+        return 'Invalid id & passkey combo'
 
 @app.route('/post', methods=['POST'])
 def result():
@@ -45,28 +63,12 @@ def result():
 
 @app.route('/times', methods=['POST','GET'])
 def times():
+    if (err:= check_login(request)) != None:
+        return err
+
     if request.method == 'GET':
         # py 3.8 assign and use
         print(request.values)
-        
-        # Check if users request has the dispenser id
-        if (str_id := request.values.get('id')) == None:
-            return 'No ID provided'
-
-        # Attempt to decode dispenser id to base10 from str hex
-        try:
-            id = int(str_id,16)
-        except:
-            return 'Invalid dispenser id format'
-        # Check if users request has the passkey
-        if (passkey := request.values.get('pass')) == None:
-            return 'No pass provided'
-        # Check if the dispenser id matches an existing dispenser
-        if (dispenser:= db.session.get(Dispensers, id)) == None:
-            return 'Invalid dispenser id'
-        # Check if the actual dispenser's key matches that provided by the user
-        if (dispenser.passkey != passkey):
-            return 'Invalid id & passkey combo'
         # All success = attempt to 
         return 'success'
 
