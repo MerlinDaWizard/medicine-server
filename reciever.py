5839ec1f5a64930033b6011dff515945c2b00412
Merlin <merlinhancock@gmail.com>
2022-10-26 15:26:58 +0100

    Post times

--- reciever.py
+++ reciever.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, Request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime,time
 import sqlalchemy as sa
-from typing import Optional
+from typing import Union
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dispensers.db'
@@ -11,49 +11,52 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dispensers.db'
 db = SQLAlchemy(app) # db. = sa. but sa gives annotations
 
 #db.init_app(app)
-class Dispensers(db.Model):
-    __tablename__ = "dispensers_table"
+class Dispenser(db.Model):
+    __tablename__ = "dispenser_table"
 
     id = sa.Column(sa.Integer, primary_key=True)
     passkey = sa.Column(sa.String(10), nullable=False)
-    times = db.relationship("ScheduleTimes", backref="dispensers_table")
+    times = db.relationship("ScheduleTime", backref="dispenser_table")
     latest_connection = sa.Column(sa.DateTime, nullable=False)
 
     def __repr__(self) -> str:
         return f"<name {self.id}>"
 
 
-class ScheduleTimes(db.Model):
-    __tablename__ = "schedule_times_table"
+class ScheduleTime(db.Model):
+    __tablename__ = "schedule_time_table"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    dispenser_id = sa.Column(sa.Integer, sa.ForeignKey("dispensers_table.id"))
+    dispenser_id = sa.Column(sa.Integer, sa.ForeignKey("dispenser_table.id"))
     time = sa.Column(sa.Time, nullable=False)
 
     def __repr__(self) -> str:
         return f"<name {self.id}>"
 
 
-def check_login(request: Request) -> Optional[bool]:
+def check_login(request: Request) -> Union[Dispenser,str]:
     '''Check info provided in request is correct to login'''
     # Check if users request has the dispenser id
-    if (str_id := request.values.get('id')) == None:
+    if (str_id := request.values.get('id')) is None:
         return 'No ID provided'
+    # Check if users request has the passkey
+    if (passkey := request.values.get('pass')) is None:
+        return 'No pass provided'
 
     # Attempt to decode dispenser id to base10 from str hex
     try:
         id = int(str_id,16)
     except:
         return 'Invalid dispenser id format'
-    # Check if users request has the passkey
-    if (passkey := request.values.get('pass')) == None:
-        return 'No pass provided'
+        
     # Check if the dispenser id matches an existing dispenser
-    if (dispenser:= db.session.get(Dispensers, id)) == None:
+    if (dispenser:= db.session.get(Dispenser, id)) is None:
         return 'Invalid dispenser id'
     # Check if the actual dispenser's key matches that provided by the user
     if (dispenser.passkey != passkey):
         return 'Invalid id & passkey combo'
+
+    return dispenser
 
 @app.route('/post', methods=['POST'])
 def result():
@@ -63,17 +66,32 @@ def result():
 
 @app.route('/times', methods=['POST','GET'])
 def times():
-    if (err:= check_login(request)) != None:
-        return err
+    dispenser_or_err = check_login(request)
+    if type(dispenser_or_err) is str: # If it returns a string it is an error to be sent back
+        return dispenser_or_err
+    else:
+        dispenser = dispenser_or_err
 
     if request.method == 'GET':
         # py 3.8 assign and use
         print(request.values)
-        # All success = attempt to 
         return 'success'
 
     elif request.method == 'POST':
-        return 'NYI'
+        if (user_time:= request.values.get('time')) is None:
+            return 'No time provided.'
+        try:
+            user_time = time.fromisoformat(user_time) # Format = HH[:MM[:SS[.fff[fff]]]][+HH:MM[:SS[.ffffff]]] eg. 04:23:01
+        except:
+            return 'Invalid time format'
+
+        schedule_element = ScheduleTime(dispenser_id=dispenser.id,time=user_time)
+        #try:
+        db.session.add(schedule_element)
+        db.session.commit()
+        #except:
+        #    return 'database commit failure'
+        return 'POST success'
 
 
 with app.app_context():
